c27e3ec4306d1996a2dd8721490fb2af82b0726e
stepankarelin <stepan.karelin.1996@mail.ru>
2023-01-16 00:27:49 +0600

    Pixel perfect 1024px adaptive, added <picture>, swiper media + pagination bullet/fraction

--- blanchard/js/swiper-prop.js
+++ blanchard/js/swiper-prop.js
@@ -2,9 +2,6 @@ const heroSwiper = document.querySelector('.hero__swiper');
 const galerySwiper = document.querySelector('.galery__swiper');
 const eventsSwiper = document.querySelector('.events__swiper');
 const projectsSwiper = document.querySelector('.projects__swiper');
-const galeryCounterPerv = document.querySelector('.galery-button-prev');
-const galeryCounterNext = document.querySelector('.galery-button-next');
-const galeryCounterText = document.querySelector('.galery__counter');
 
 const swiperHero = new Swiper(heroSwiper, {
   containerModifierClass: 'hero__swiper',
@@ -22,8 +19,20 @@ const swiperHero = new Swiper(heroSwiper, {
 })
 
 const swiperGalery = new Swiper(galerySwiper, {
-  slidesPerView: 3,
-  slidesPerGroup: 3,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+    },
+    768: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+    1025: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+    },
+  },
   containerModifierClass: 'galery__swiper',
   slideClass: 'galery__slide',
   wrapperClass: 'galery__wrapper',
@@ -31,20 +40,34 @@ const swiperGalery = new Swiper(galerySwiper, {
     nextEl: '.galery-button-next',
     prevEl: '.galery-button-prev',
   },
-})
-
-galeryCounterNext.addEventListener('click', e => {
-  galeryCounterText.textContent = '2 / 2'
-})
-
-galeryCounterPerv.addEventListener('click', e => {
-  galeryCounterText.textContent = '1 / 2'
+  pagination: {
+    el: '.galery-pagination',
+    type: 'fraction',
+  },
 })
 
 const swiperEvents = new Swiper(eventsSwiper, {
-  slidesPerView: 3,
-  slidesPerGroup: 3,
   spaceBetween: 50,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+    },
+    769: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 27,
+    },
+    1025: {
+      spaceBetween: 50,
+    },
+  },
+  pagination: {
+    el: '.events-pagination',
+    type: 'bullets',
+    bulletClass: 'events__pagination',
+    bulletActiveClass: 'events__pagination--active',
+  },
   containerModifierClass: 'events__swiper',
   slideClass: 'events__slide',
   wrapperClass: 'events__wrapper',
@@ -55,8 +78,20 @@ const swiperEvents = new Swiper(eventsSwiper, {
 })
 
 const swiperProjects = new Swiper(projectsSwiper, {
-  slidesPerView: 3,
-  slidesPerGroup: 3,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+    },
+    769: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+    1025: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+    },
+  },
   spaceBetween: 50,
   containerModifierClass: 'projects__swiper',
   slideClass: 'projects__slide',
